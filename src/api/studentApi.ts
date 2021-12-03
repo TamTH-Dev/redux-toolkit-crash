be6f400dd937711ed17fe5d31920acb7d32b4539
TamTH-Dev <tamth.dev@gmail.com>
2021-12-03 22:13:38 +0700

    Add student pi

--- src/api/studentApi.ts
+++ src/api/studentApi.ts
@@ -1,10 +1,27 @@
-import axiosClient from "./axiosClient"
+import axiosClient from './axiosClient'
+import { Student, ListResponse, ListParams } from '../models'
 
-const studentApi = {
-  getAll() {
-    const url = '/cities'
-    return axiosClient.get(url)
-  }
+const cityApi = {
+  async getAll(params: ListParams): Promise<ListResponse<Student>> {
+    const url = '/students'
+    return await axiosClient.get(url, { params })
+  },
+  async getById(id: string): Promise<Student> {
+    const url = `/students/${id}`
+    return await axiosClient.get(url)
+  },
+  async add(data: Student): Promise<Student> {
+    const url = '/students'
+    return await axiosClient.post(url, data)
+  },
+  async update(id: string, data: Student): Promise<Student> {
+    const url = `/students/${id}`
+    return await axiosClient.patch(url, data)
+  },
+  async remove(id: string): Promise<any> {
+    const url = `/students/${id}`
+    return await axiosClient.delete(url)
+  },
 }
 
-export default studentApi
+export default cityApi
